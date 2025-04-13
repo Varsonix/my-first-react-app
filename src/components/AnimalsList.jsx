1a3446d126d98098c2133ecdba54a2119af1a69b
Jordan Gibbard <46365880+Varsonix@users.noreply.github.com>
2025-04-13 11:20:17 -0700

    Better understanding of props

--- src/components/AnimalsList.jsx
+++ src/components/AnimalsList.jsx
@@ -1,24 +1,28 @@
-function ListItem(props) {
-    return <li>{props.animal}</li>
-  }
-  
-  function List(props) {
-    return (
-      <ul>
-        {props.animals.map((animal) => {
-          return <ListItem key={animal} animal={animal} />;
-        })}
-      </ul>
-    )
+function List(props) {
+  if (!props.animals) {
+    return <div>Loading...</div>;
   }
 
+  if (props.animals.length === 0) {
+    return <div>There are no animals in the list!</div>;
+  }
+
+  return (
+    <ul>
+      {props.animals.map((animal) => {
+        return <li key={animal}>{animal}</li>;
+      })}
+    </ul>
+  );
+}
+
 export default function AnimalsList(){
-    const animals = ["Lion", "Cow", "Snake", "Lizard"]
+    const animals = []
 
     return (
         <div>
           <h1>Animals: </h1>
-          <List animals={animals} />
+          <List animals={animals}/>
         </div>
       )
 }

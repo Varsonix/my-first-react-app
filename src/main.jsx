5ce29ef9848d198367ac213f8afdb4ca539bb6aa
Jordan Gibbard <46365880+Varsonix@users.noreply.github.com>
2025-04-13 11:05:13 -0700

    Refactoring

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import ExerciseOne from './components/ExerciseOne'
+import App from './App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ExerciseOne />
-    {/* <ButtonTest /> */}
+    <App />
   </StrictMode>
 )

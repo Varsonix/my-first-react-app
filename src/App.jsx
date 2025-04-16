86cc4b46a21e2513f071448a9eb33b507e2cd591
Jordan Gibbard <46365880+Varsonix@users.noreply.github.com>
2025-04-15 19:34:14 -0700

    Updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css'
 import Gallery from './components/Lessons/Exercises/Statues/SculptureList'
+import Form from './components/Lessons/Exercises/FormInput/FormTest'
+import FeedbackForm from './components/Lessons/Exercises/FormInput/FormCrashFix'
 
 function App() {
   return (
-    <Gallery />
+    <div className="Page">
+      <FeedbackForm />
+    </div>
   )
 }
 
--- src/components/Lessons/Exercises/Statues/SculptureList.jsx
+++ src/components/Lessons/Exercises/Statues/SculptureList.jsx
@@ -4,15 +4,51 @@ import { sculptureList } from './data.jsx';
 export default function Gallery() {
   // index is a state, and setIndex is a setter function
   const [index, setIndex] = useState(0);
+  const [showMore, setShowMore] = useState(false);
+  // 3. Your component’s second render. React still sees useState(0), but because React remembers that you set index to 1, it returns [1, setIndex] instead.
 
-  function handleClick() {
-    setIndex(index + 1)
+  // When you call useState, you are telling React that you want this component to remember something:
+  // In this case, you want React to remember index.
+
+  // The convention is to name this pair like const [something, setSomething]. 
+  // You could name it anything you like, but conventions make things easier to understand across projects.
+
+
+  // 1. Your component renders the first time. Because you passed 0 to useState as the initial value for index, it will return [0, setIndex]. React remembers 0 is the latest state value.
+
+  let hasPrev = index > 0;
+  let hasNext = index < sculptureList.length - 1;
+
+  function handleNextClick() {
+    if (hasNext){
+      setIndex(index + 1)
+    }
+      // 2. You update the state. When a user clicks the button, it calls setIndex(index + 1). index is 0, so it’s setIndex(1). This tells React to remember index is 1 now and triggers another render.
+  }
+
+  function handlePrevClick() {
+    if (hasPrev){
+      setIndex(index - 1);
+    }
+  }
+
+  function handleMoreClick() {
+      setShowMore(!showMore)
   }
 
   let sculpture = sculptureList[index];
   return (
-    <>
-      <button onClick={handleClick}>
+<>
+      <button
+        onClick={handlePrevClick}
+        disabled={!hasPrev}
+      >
+        Previous
+      </button>
+      <button
+        onClick={handleNextClick}
+        disabled={!hasNext}
+      >
         Next
       </button>
       <h2>
@@ -22,13 +58,14 @@ export default function Gallery() {
       <h3>  
         ({index + 1} of {sculptureList.length})
       </h3>
+      <button onClick={handleMoreClick}>
+        {showMore ? 'Hide' : 'Show'} details
+      </button>
+      {showMore && <p>{sculpture.description}</p>}
       <img 
         src={sculpture.url} 
         alt={sculpture.alt}
       />
-      <p>
-        {sculpture.description}
-      </p>
     </>
   );
 }

c6b2f318fddd44e71005e44b1e2d0d86f7fc6af2
Jordan Gibbard <46365880+Varsonix@users.noreply.github.com>
2025-04-16 17:12:38 -0700

    Exercise

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import './App.css'
 import Gallery from './components/Lessons/Exercises/Statues/SculptureList'
 import Form from './components/Lessons/Exercises/FormInput/FormTest'
 import FeedbackForm from './components/Lessons/Exercises/FormInput/FormCrashFix'
+import TrafficLight from './components/Lessons/Exercises/StreetLights/Light'
 
 function App() {
   return (
     <div className="Page">
-      <FeedbackForm />
+      <TrafficLight />
     </div>
   )
 }

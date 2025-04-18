1b918872d292173b4cc74f49af29ec015e5faa3c
Jordan Gibbard <46365880+Varsonix@users.noreply.github.com>
2025-04-18 11:27:06 -0700

    New exercise

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import Gallery from './components/Lessons/Exercises/Statues/SculptureList'
 import Form from './components/Lessons/Exercises/FormInput/FormTest'
 import FeedbackForm from './components/Lessons/Exercises/FormInput/FormCrashFix'
 import TrafficLight from './components/Lessons/Exercises/StreetLights/Light'
+import Clock from './components/Lessons/Exercises/ClockUpdate/Clock'
+import Contact from './components/Lessons/Exercises/Authorization/Authorization'
 
 function App() {
+  const currentTime = new Date().toLocaleTimeString();
+
   return (
     <div className="Page">
-      <TrafficLight />
+      <Contact />
     </div>
   )
 }

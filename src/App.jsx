31f2c041425bbc9285b0b71d44628fd26c0ee76e
Jordan Gibbard <46365880+Varsonix@users.noreply.github.com>
2025-04-18 21:23:38 -0700

    Concepts

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import './App.css'
-import Gallery from './components/Lessons/Exercises/Statues/SculptureList'
-import Form from './components/Lessons/Exercises/FormInput/FormTest'
-import FeedbackForm from './components/Lessons/Exercises/FormInput/FormCrashFix'
-import TrafficLight from './components/Lessons/Exercises/StreetLights/Light'
-import Clock from './components/Lessons/Exercises/ClockUpdate/Clock'
-import Contact from './components/Lessons/Exercises/Authorization/Authorization'
+import ColorPicker from './components/Lessons/Basic Concepts/useState/ColorPicker'
 
 function App() {
   const currentTime = new Date().toLocaleTimeString();
 
   return (
     <div className="Page">
-      <Contact />
+      <ColorPicker />
     </div>
   )
 }

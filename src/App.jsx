fe16800bd29a1440a5e3e151de67d83d4fdb87a6
Jordan Gibbard <46365880+Varsonix@users.noreply.github.com>
2025-04-22 21:50:37 -0700

    Exerc

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
 import ColorPicker from './components/Lessons/Basic Concepts/useState/ColorPicker'
+import EditProfile from './components/Lessons/Exercises/FormSubmittalExample/FormSubmit';
 
 function App() {
   const currentTime = new Date().toLocaleTimeString();
 
   return (
     <div className="Page">
-      <ColorPicker />
+      <EditProfile />
     </div>
   )
 }

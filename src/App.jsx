4451246ef2e8ae237984557095e53f5635e633b1
Jordan Gibbard <46365880+Varsonix@users.noreply.github.com>
2025-04-29 00:01:24 -0700

    UseEffect learning

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import './App.css'
 import ColorPicker from './components/Lessons/Basic Concepts/useState/ColorPicker'
 import EditProfile from './components/Lessons/Exercises/FormSubmittalExample/FormSubmit';
+import Counter from './components/Lessons/Exercises/useEffect/useEffectTesting';
 
 function App() {
   const currentTime = new Date().toLocaleTimeString();
 
   return (
-    <div className="Page">
-      <EditProfile />
-    </div>
+    <Counter />
   )
+
 }
 
 export default App
